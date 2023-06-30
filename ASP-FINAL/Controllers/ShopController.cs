aced3eb0b1ac4c43a5ae5ee6178e8f8af1a41c01
Murad Jafarov <muradjj@code.edu.az>
2023-06-30 23:17:27 +0400

    Backup is ready

--- ASP-FINAL/Controllers/ShopController.cs
+++ ASP-FINAL/Controllers/ShopController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASP_FINAL.Data;
+using ASP_FINAL.Models;
+using ASP_FINAL.Services.Interfaces;
+using ASP_FINAL.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASP_FINAL.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductService _productService;
+        private readonly ISettingService _settingService;
+        private readonly ICategoryService _categoryService;
+        private readonly ISubcategoryService _subcategoryService;
+        private readonly AppDbContext _context;
+
+
+
+        public ShopController(IProductService productService,
+                                     ISettingService settingService,
+                                     ICategoryService categoryService,
+                                     AppDbContext context,
+                                     ISubcategoryService subcategoryService)
         {
-            return View();
+            _productService = productService;
+            _settingService = settingService;
+            _categoryService = categoryService;
+            _subcategoryService = subcategoryService;
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+
+            IEnumerable<Product> product = await _productService.GetAllWithIncludesAsync();
+
+            LayoutVM model = new LayoutVM
+            {
+                Products = product.ToList(),
+            };
+
+            return View(model);
         }
     }
 }
